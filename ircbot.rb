20c546d08622cdc263ba3620b5a9957119142640
Karl-Martin Skontorp <kms@skontorp.net>
2007-05-03 10:48:18 +0000

    A    .
    	*) Initial import of TetriNET IRC bot.
    
    
    git-svn-id: svn+ssh://ginandtonic.no/home/kms/.svnrepos/tetrinetbot@725 bec0a6a6-59ba-0310-824c-86e0bd1f4ed1

---
+++ ircbot.rb
@@ -0,0 +1,74 @@
+require "socket"
+
+module IRC
+
+  class Bot
+
+    def initialize(config)
+      @nick, @username, @realname, @host, @server = config["nick"], config["username"],
+        config["realname"], config["host"], config["server"]
+    end
+
+    def run
+      if @host
+        @sock = TCPSocket.new(@server["host"], @server["port"], @host)
+      else
+        @sock = TCPSocket.new(@server["host"], @server["port"])
+      end
+      thread = Thread.new {
+        loop {
+          break unless reply = @sock.gets
+          cmd, sender, target, args = "", "", "", []
+          reply.sub(/^(?::(\S+)\s+)?(\S+)\s+(?::?(\S+))\s*((?:[^:]\S*\s*)*)?(?::(.+))?$/) {
+            sender = $1
+            cmd = "on_" + $2.downcase
+            target = $3
+            args = $4.split if $4
+            args << $5.chop if $5
+          }
+          sender = sender ? (sender.include?(?!) ? sender.split(/!|@/) : [sender]) : [@server["host"]]
+          send(cmd, sender, target, args) if respond_to?(cmd)
+        }
+      }
+      do_nick(@nick)
+      do_user(@username, @host ? @host : "localhost", @server["host"], @realname)
+      thread
+    end
+
+    def sendq(string)
+      @sock.puts string if @sock
+    end
+
+    # event handlers
+
+    def on_ping(sender, target, args)
+      sendq "PONG :" + target
+    end
+
+    def on_433(sender, target, args)
+      # somebody stole our nick! omg!
+      do_nick(@nick + "_")
+    end
+
+    # commands
+
+    def do_user(username, hostname, server, realname)
+      sendq "USER #{username} #{hostname} #{server} :#{realname}"
+    end
+
+    def do_nick(nick)
+      sendq "NICK #{nick}"
+    end
+
+    def do_join(channel)
+      sendq "JOIN #{channel}"
+    end
+
+    def do_privmsg(channel, msg)
+      sendq "PRIVMSG #{channel} :#{msg}"
+    end
+
+  end
+
+end
+
---
+++ tetrinetbot.rb
@@ -0,0 +1,76 @@
+#!/usr/bin/env ruby
+
+require 'ircbot'
+require 'yaml'
+require 'pp'
+require 'socket'
+
+class Tetrinetbot < IRC::Bot
+    def initialize(config)
+	@channel = config["channel"]
+	super
+    end
+
+    def on_001(sender, target, args)
+	# connection is established when 001 is received
+	# hijack it to autojoin channels
+	do_join(@channel)
+    end
+
+    def on_privmsg(sender, target, args)
+
+    end
+
+    def on_join(sender, target, args)
+	puts "#{sender[0]} JOIN #{target}"
+    end
+end
+
+config = YAML::load(File.open("tetrinetbot.conf"))
+
+bot = Tetrinetbot.new(config)
+
+threads = []
+
+threads << bot.run
+threads << Thread.new {
+    alertsent = false
+    while true
+	sock = TCPSocket.open(config["tetrinethost"], config["tetrinetport"])
+	sock.send("listuser\n", 0)
+	sleep(0.1) # Jallaballa
+	sock.send("\255\n", 0)
+	r = sock.readlines
+	sock.close
+
+	okLine = r.index("+OK\n")
+
+	if okLine == nil
+	    print "Bad data from tetrinet server\n"
+	else
+	    users = []
+	    r[0,okLine].each { |l|
+		l.sub(/^\"(.*?)\"/) {
+		    users << $1
+		}
+	    }
+
+	    pp users
+
+	    if users.size == 1
+		if alertsent == false
+		    bot.do_privmsg(config["channel"], "TetriNET single-user ALERT! [%s]" % users[0])
+		    alertsent = true
+		end
+	    else 
+		alertsent = false
+	    end
+	end
+
+	sleep(2)
+    end
+}
+
+threads.each { |t| 
+    t.join 
+}
